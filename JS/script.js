dcf0a3d3cde272e043e7f63c3aea00c94f001a31
Jeevan <prateeklenka286@gmail.com>
2021-01-25 11:31:09 +0530

    added JS files

---
+++ JS/script.js
@@ -0,0 +1,26 @@
+const texts = ["YouTuber", "Developer", "Data Scientist", "Photographer", "Freelancer", "Designer", "Creator"];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type() {
+    if(count === texts.length){
+        count = 0;
+    }
+
+    currentText  = texts[count];
+    letter = currentText.slice(0, ++index);
+    
+
+    document.querySelector('.typing').textContent = letter;
+    if(letter.length === currentText.length){
+        count++;
+        index = 0;
+    }
+
+    setTimeout(type, 400);
+
+}());
+
+
